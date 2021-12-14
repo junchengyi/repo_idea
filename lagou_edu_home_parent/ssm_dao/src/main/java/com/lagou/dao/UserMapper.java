c74fd7d5f9b35a7a487ec0c43f968dae9e8385a4
liujunqin <1369928668@qq.com>
2021-12-14 11:01:39 +0800

    third change11

--- lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -36,31 +36,6 @@ public interface UserMapper {
      * 4.获取用户拥有的资源权限信息
      * */
     public List<Resource> findResourceByRoleId(List<Integer> ids);
-    
-     /**
-     * 4.获取用户拥有的资源权限信息
-     * */
-    public List<Resource> findResourceByRoleId2(List<Integer> ids);
-    
-    
 
-    public void test11();
-    public void test21();
-    public void test31();
-    public void test41();
-    public void test51();
-    public void test61();
-    public void test71();
-
-    public void test1();
-    public void test2();
-    public void test3();
-    public void test4();
-    public void test5();
-    public void test6();
-    public void test7();
-
-
-    public void test1111();
 
 }
